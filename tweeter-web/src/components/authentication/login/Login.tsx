29a60d13b634fe0434a5c5793df6fa8e8a754f19
Tyler Trommlitz <ttrommlitz6@gmail.com>
2024-01-10 12:26:14 -0700

    initial files

---
+++ tweeter-web/src/components/authentication/login/Login.tsx
@@ -0,0 +1,113 @@
+import "./Login.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { useContext } from "react";
+import { UserInfoContext } from "../../userInfo/UserInfoProvider";
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthenticationFormLayout from "../AuthenticationFormLayout";
+import { AuthToken, FakeData, User } from "tweeter-shared";
+import useToastListener from "../../toaster/ToastListenerHook";
+
+interface Props {
+  originalUrl?: string;
+}
+
+const Login = (props: Props) => {
+  const [alias, setAlias] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const navigate = useNavigate();
+  const { updateUserInfo } = useContext(UserInfoContext);
+  const { displayErrorMessage } = useToastListener();
+
+  const rememberMeRef = useRef(rememberMe);
+  rememberMeRef.current = rememberMe;
+
+  const checkSubmitButtonStatus = (): boolean => {
+    return !alias || !password;
+  };
+
+  const doLogin = async () => {
+    try {
+      let [user, authToken] = await login(alias, password);
+
+      updateUserInfo(user, user, authToken, rememberMeRef.current);
+
+      if (!!props.originalUrl) {
+        navigate(props.originalUrl);
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      displayErrorMessage(
+        `Failed to log user in because of exception: ${error}`
+      );
+    }
+  };
+
+  const login = async (
+    alias: string,
+    password: string
+  ): Promise<[User, AuthToken]> => {
+    // TODO: Replace with the result of calling the server
+    let user = FakeData.instance.firstUser;
+
+    if (user === null) {
+      throw new Error("Invalid alias or password");
+    }
+
+    return [user, FakeData.instance.authToken];
+  };
+
+  const inputFieldGenerator = () => {
+    return (
+      <>
+        <div className="form-floating">
+          <input
+            type="text"
+            className="form-control"
+            size={50}
+            id="aliasInput"
+            placeholder="name@example.com"
+            onChange={(event) => setAlias(event.target.value)}
+          />
+          <label htmlFor="aliasInput">Alias</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="password"
+            className="form-control bottom"
+            id="passwordInput"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <label htmlFor="passwordInput">Password</label>
+        </div>
+      </>
+    );
+  };
+
+  const switchAuthenticationMethodGenerator = () => {
+    return (
+      <div className="mb-3">
+        Not registered? <Link to="/register">Register</Link>
+      </div>
+    );
+  };
+
+  return (
+    <AuthenticationFormLayout
+      headingText="Please Sign In"
+      submitButtonLabel="Sign in"
+      oAuthHeading="Sign in with:"
+      inputFieldGenerator={inputFieldGenerator}
+      switchAuthenticationMethodGenerator={switchAuthenticationMethodGenerator}
+      setRememberMe={setRememberMe}
+      submitButtonDisabled={checkSubmitButtonStatus}
+      submit={doLogin}
+    />
+  );
+};
+
+export default Login;
